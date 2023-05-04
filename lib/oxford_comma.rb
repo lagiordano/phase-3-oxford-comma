f17770b8612cbef7d71a9d622cf9e4894fab0ce1
Lauren Giordano <laurengiordano94@gmail.com>
2023-05-04 15:22:39 +1000

    commit tree 77315235ee61e6257af96d5b0349e0e2dc348b22

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+    if array.length == 1 
+        array.join("")
+    elsif array.length == 2
+        array.insert(1, "and")
+        array.join(" ")
+    elsif array.length > 2
+        array.insert(-2, "and ")
+        final_element = array.pop
+        puts "#{array}"
+        puts "#{final_element}"
+        new_string = array.join(", ") + final_element
+        new_string
+    end
 end
+
+oxford_comma ["fiddleheads", "okra", "kohlrabi"]
